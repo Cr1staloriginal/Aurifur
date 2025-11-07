3bf6dd7c2a9cea2f32beb02183a494a49949f3a1
Cr1stalik ˢˢ <dmitrijosipov877@gmail.com>
2025-11-07 09:06:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 # Инициализация бота с нужными интентами
 intents = disnake.Intents.default()
 intents.message_content = True
-bot = disnake.Client(intents=intents)
+bot = disnake.ExtBot(intents=intents, command_prefix="!")
 
 async def shutdown():
     logger.info("Bot stopped by signal")
